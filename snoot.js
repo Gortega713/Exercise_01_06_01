176753c17d18aa7cb31c8a0d3bb9777bbf900714
Unknown <Gortega713@west-mec.org>
2018-08-07 18:24:03 -0700

    Document Fragments
    Added document fragments and added a function to run all of the functions as soon as the page loads.

--- snoot.js
+++ snoot.js
@@ -10,17 +10,71 @@
 
 "use strict";
 
+// Variables that have Global/Module scope
+
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
 // Function to turn off select list defaults
 
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
-    alert("Select Lists: " + emptyBoxes.length);
+    for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;
+    }
+}
+
+// Function to set up document fragment for days of month
+
+function setUpDays() {
+    // Get the days select list in the delivery date portion of form
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
+// Function to update days selection list
+
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+
+    
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+    
+    if (deliveryMonth.selectedIndex === -1) {
+        deliveryMonth.selectedIndex = 0;
+    }
+    
+    // If Feb and 2018 - leap year - twentyNine
+    
+    // Else 30 day month - thirty
+    
+    // Else 31 month - thirtyOne
+}
+
+// Function to run on page load
+
+function setUpPage() {
+    removeSelectDefaults();
+    setUpDays();
+    updateDays();
 }
 
 // Enable load event handlers
 
-if (window.addEventListener){
-    window.addEventListener("load", removeSelectDefaults, false);
+if (window.addEventListener) {
+    window.addEventListener("load", setUpPage, false);
 } else if (window.attachEvent) {
-    window.attachEvent("onload", removeSelectDefaults);
+    window.attachEvent("onload", setUpPage);
 }
