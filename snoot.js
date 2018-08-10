34fae1ae9aff7e68582c37d615e750087f87f051
Unknown <Gortega713@west-mec.org>
2018-08-09 18:24:56 -0700

    Form Validation
    Added validation using an evt method and a global variable. Used the novalidate attribute for the form tag to turn off browser validation

--- snoot.js
+++ snoot.js
@@ -15,6 +15,7 @@
 var twentyNine = document.createDocumentFragment();
 var thirty = document.createDocumentFragment();
 var thirtyOne = document.createDocumentFragment();
+var formValidity = false;
 
 // Function to turn off select list defaults
 
@@ -74,21 +75,66 @@ function updateDays() {
     }
 }
 
-// Function too see if custom message is checked 
+// Function to see if custom message is checked 
 
 function autoCheckCustom() {
     var messageBox = document.getElementById("customText");
-    
+
     if (messageBox.value !== "" && messageBox.value !== messageBox.placeholder) {
         // Text area actually has something in it
         document.getElementById("custom").checked = "checked";
-        
+
     } else {
         // Text area has nothing in it
         document.getElementById("custom").checked = "";
-        
+
     }
 }
+
+// Function to copy delivery to billing address
+
+function copyBillingAddress() {
+    var billingInputElements = document.querySelectorAll("#billingAddress input");
+    var deliveryInputElements = document.querySelectorAll("#deliveryAddress input");
+
+    // If checkbox is checked, copy all fields
+
+    if (document.getElementById("sameAddr").checked) {
+        for (var i = 0; i < billingInputElements.length; i++) {
+            deliveryInputElements[i + 1].value = billingInputElements[i].value
+        }
+        document.querySelector("#deliveryAddress select").value = document.querySelector("#billingAddress select").value;
+    }
+
+    // Else, erase all fields
+    else {
+        for (var i = 0; i < billingInputElements.length; i++) {
+            deliveryInputElements[i + 1].value = ""
+        }
+        document.querySelector("#deliveryAddress select").selectedIndex = -1;
+    }
+}
+
+// Function to validate entire form
+
+function validateForm(evt) {
+    if(evt.preventDefault) {
+       evt.preventDefault();
+    } else {
+       evt.returnValue = false;
+    }
+    
+    if (formValidity === true) {
+        document.getElementById("errorText").innerHTML = "";
+        document.getElementById("errorText").style.display = "none";
+        document.getElementsByTagName("form")[0].submit();
+    } else {
+        document.getElementById("errorText").innerHTML = "Please fix the indicated problems and then resubmit your order";
+        document.getElementById("errorText").style.display = "block";
+        scroll(0,0);
+    }
+}
+
 
 // Function to run on page load
 
@@ -101,35 +147,56 @@ function setUpPage() {
 // Function to create our event listeners
 
 function createEventListeners() {
-    
+
     // Event listeners for Month select list
-    
+
     var deliveryMonth = document.getElementById("delivMo");
     if (deliveryMonth.addEventListener) {
-        deliveryMonth.addEventListener("change", autoCheckCustom, false);
+        deliveryMonth.addEventListener("change", updateDays, false);
     } else if (deliveryMonth.attachEvent) {
-        deliveryMonth.attachEvent("onchange", autoCheckCustom);
+        deliveryMonth.attachEvent("onchange", updateDays);
     }
-    
-    // Event Listeners for years select list
-    
+
+    // Event listener for years select list
+
     var deliveryYear = document.getElementById("delivYr");
     if (deliveryYear.addEventListener) {
         deliveryYear.addEventListener("change", updateDays, false);
     } else if (deliveryYear.attachEvent) {
         deliveryYear.attachEvent("onchange", updateDays);
     }
-    
-    // Event lister for change in custom text field
-    
+
+    // Event listener for change in custom text field
+
     var messageBox = document.getElementById("customText");
     if (messageBox.addEventListener) {
         messageBox.addEventListener("change", autoCheckCustom, false);
     } else if (messageBox.attachEvent) {
         messageBox.attachEvent("onchange", autoCheckCustom);
     }
+    
+    // Event listener for checkbox to copy data 
 
+    var same = document.getElementById("sameAddr");
+    if (same.addEventListener) {
+        same.addEventListener("change", copyBillingAddress, false);
+    } else if (same.attachEvent) {
+        same.attachEvent("onchange", copyBillingAddress);
+    }
+    
+    // Event listener for validation on form submission
+    
+    var form = document.getElementsByTagName("form")[0];
+    if (form.addEventListener) {
+        form.addEventListener("submit", validateForm, false);
+    } else if (form.attachEvent) {
+        form.attachEvent("onsubmit", validateForm);
+    }
 }
+
+
+
+
 
 // Enable load event handlers
 
